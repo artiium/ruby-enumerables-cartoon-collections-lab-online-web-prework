e8d66d5621156ec4c483c96fe4e8a93e41d0a70c
Artiium Arbyummi <artiium21@gmail.com>
2019-09-10 02:42:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(roll_call)
-  roll_call.each_with_index do |roll, index|
-  puts "#{index+1}#{roll}"
+  roll_call.each_with_index do |dwarf, index|
+  puts "#{index+1}#{dwarf}"
 end
 end
 
